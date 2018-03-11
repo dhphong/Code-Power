92f222f7fd7c07f332880ef4977f8c750f39ad88
Do Phong <phong987phong@gmail.com>
2018-03-11 16:42:32 +0700

    Commit TongBa

--- Tuan4/TongBa.cpp
+++ Tuan4/TongBa.cpp
@@ -6,26 +6,54 @@
 
 using namespace std;
 
+void findTriplets(vector<int> arr)
+{
+ 	int n = arr.size();
+    sort(arr.begin(), arr.end());
+
+    for (int i=0; i<n-2 && arr[i] <= 0; i++)
+    {
+    	if (i > 0 && arr[i] == arr[i - 1])
+    		continue;
+
+        int l = i + 1;
+        int r = n - 1;
+        int x = arr[i];
+        set<tuple<int, int>> printed;
+        while (l < r)
+        {
+        	if (l > i + 1 && arr[l] == arr[l - 1]) {
+                l++;
+                continue;
+            }
+            if (r < n - 1 && arr[r] == arr[r] + 1) {
+                r--;
+                continue;
+            }
+            if (x + arr[l] + arr[r] == 0)
+            {
+            	if (printed.find(tuple<int, int>(arr[l], arr[r])) == printed.end()){
+	                cout << x << " " <<  arr[l] << " " << arr[r] << endl;
+	                printed.insert(tuple<int, int>(arr[l], arr[r]));
+                }
+                l++;
+                r--;
+            } else if (x + arr[l] + arr[r] < 0)
+                l++;
+
+            else
+                r--;
+        }
+    }
+}
+
+
 int main() {
     int n;
     cin >> n;
     vector<int> nums(n);
-    for (int& x : nums)
+    for (int &x : nums)
         cin >> x;
-    sort(nums.begin(), nums.end());
-    set< tuple<int, int> > a;
-    for (int i = 0; i < n-2 && nums[i] <= 0; ++i) {
-        for (int j = i + 1; j < n - 1; j++) {
-            int sum = nums[i] + nums[j];
-            if (find(nums.begin() + j + 1, nums.end(), -sum) != nums.end()) {
-                if (find(a.begin(), a.end(), tuple<int, int>(nums[i], nums[j])) != a.end()) {
-                    continue;
-                } else {
-                    cout << nums[i] << " " << nums[j] << " " << -sum << endl;
-                    a.insert(tuple<int, int>(nums[i], nums[j]));
-                }
-            }
-        }
-    }
+    findTriplets(nums);
     return 0;
 }
